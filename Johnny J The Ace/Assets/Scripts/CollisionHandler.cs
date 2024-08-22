818abbd862077442037c37f9460dad3fea10f8b6
Jozka Siregar <jozkasiregar@gmail.com>
2024-08-22 11:21:55 +0700

    aircraft crash

--- Johnny J The Ace/Assets/Scripts/CollisionHandler.cs
+++ Johnny J The Ace/Assets/Scripts/CollisionHandler.cs
@@ -6,29 +6,43 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private float loadDelay = 1f;
+    [SerializeField] private ParticleSystem explosionFx;
+    [SerializeField] private GameObject aircraftBody;
     private int currentScene;
-    PlayerControls plc;
+    private PlayerControls plc;
+    private BoxCollider coll;
 
     void Start()
     {
         plc = GetComponent<PlayerControls>();
+        coll = GetComponent<BoxCollider>();
     }
 
     private void OnCollisionEnter(Collision col)
     {
         Debug.Log(this.name + " just bumped into " + col.gameObject.name);
-        Invoke("PlayerCrash", loadDelay);
+        PlayerCrash();
+        Invoke("ReloadScene", loadDelay);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} triggered by {other.gameObject.name}");
-        Invoke("PlayerCrash", loadDelay);
+        PlayerCrash();
+        Invoke("ReloadScene", loadDelay);
     }
 
     private void PlayerCrash()
     {
+        explosionFx.Play();
+        aircraftBody.SetActive(false);
         plc.enabled = false;
+        coll.enabled = false;
+
+    }
+
+    private void ReloadScene()
+    {
         currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
     }
