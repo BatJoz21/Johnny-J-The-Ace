bc85066c6541c871023326324d8666deda693761
Jozka Siregar <jozkasiregar@gmail.com>
2024-08-22 10:28:43 +0700

    Player can eliminate enemy

--- Johnny J The Ace/Assets/Scripts/CollisionHandler.cs
+++ Johnny J The Ace/Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float loadDelay = 1f;
+    private int currentScene;
+    PlayerControls plc;
+
+    void Start()
+    {
+        plc = GetComponent<PlayerControls>();
+    }
+
     private void OnCollisionEnter(Collision col)
     {
         Debug.Log(this.name + " just bumped into " + col.gameObject.name);
+        Invoke("PlayerCrash", loadDelay);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+        Invoke("PlayerCrash", loadDelay);
+    }
+
+    private void PlayerCrash()
+    {
+        plc.enabled = false;
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
---
+++ Johnny J The Ace/Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("I'm hit!");
+        Destroy(this.gameObject);
+    }
+}
