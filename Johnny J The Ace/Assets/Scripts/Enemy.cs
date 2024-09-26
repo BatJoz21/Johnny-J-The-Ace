c151daa0317bb69c058188e51efad2a8eff7b6f7
Jozka Siregar <jozkasiregar@gmail.com>
2024-09-26 20:59:03 +0700

    Add new enemies

--- Johnny J The Ace/Assets/Scripts/Enemy.cs
+++ Johnny J The Ace/Assets/Scripts/Enemy.cs
@@ -7,14 +7,23 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int enemyHealth = 3;
     [SerializeField] private GameObject enemyExplosion;
     [SerializeField] private GameObject enemyDamaged;
-    [SerializeField] private Transform parent;
     [SerializeField] private int enemyVal = 10;
 
+    private GameObject parent;
     private ScoreBoard scoreBoard;
+    private Rigidbody rb;
 
     void Awake()
     {
+        parent = GameObject.FindWithTag("SpawnAtRuntime");
         scoreBoard = FindFirstObjectByType<ScoreBoard>();
+        AddRigidBody();
+    }
+
+    private void AddRigidBody()
+    {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
@@ -45,6 +54,6 @@ public class Enemy : MonoBehaviour
     private void PlayParticleEffect(GameObject val)
     {
         GameObject vfx = Instantiate(val, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parent.transform;
     }
 }
